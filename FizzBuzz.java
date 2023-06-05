3be4d93dfd436b939b2d4fea8eae9ecdeb32a148
HaydenChan8619 <haydenphchan@gmail.com>
2023-06-04 23:46:44 -0700

    This is the completed code for FizzBuzz.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,6 +5,37 @@ public class FizzBuzz {
     // else if multiple of 3, print "Fizz"
     // else if multiple of 7, print "Buzz"
     // else print number
+    //static int test = 10;
+    static void FizzBuzzDisplay() {
 
+        int counter = 1;
 
+        while (counter < 101){
+
+            if (counter % 3 == 0 && counter % 7 == 0){
+
+                System.out.println("FizzBuzz");
+
+            } else if (counter % 3 == 0){
+                System.out.println("Fizz");
+
+            } else if (counter % 7 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(counter);
+            }
+
+            counter = ++counter;
+
+        }
+
+        //test = ++test;
+
+        //System.out.println(test);
+
+    }
+
+    public static void main(String[] args) {
+        FizzBuzzDisplay();
+    }
 }
