eda9891b574241a6aeea2b19ced7b0e1a55baf34
HaydenChan8619 <haydenphchan@gmail.com>
2023-06-05 14:53:05 -0700

    This is the completed code of WordReversal.

--- WordReversal.java
+++ WordReversal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner; // scanner class used to acquire user input
+
 public class WordReversal {
 
     // The following code will flip the order of the words in an inputted sentence (based on spaces)
     // Example: Input "Dog is good" ; Output "good is Dog"
+
+    public static void main(String[] args){
+
+        Scanner userInput = new Scanner(System.in);
+        String sentence;
+
+        System.out.println("Type a sentence to be reversed!");
+
+        sentence = userInput.nextLine();
+
+        String[] stringArrayed; //user input split into each word
+
+        stringArrayed = sentence.split(" ");
+
+        for (int i = stringArrayed.length; i > 0; i--){
+             System.out.print(stringArrayed[i-1] + " ");
+        }
+    }
 }
