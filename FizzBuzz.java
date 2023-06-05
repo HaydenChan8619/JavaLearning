d1bce82dcee37bccb8f8ea5ed1d4babb4a9cb6d7
HaydenChan8619 <haydenphchan@gmail.com>
2023-06-04 22:49:12 -0700

    This is the first commit with the comments written.

---
+++ FizzBuzz.java
@@ -0,0 +1,10 @@
+public class FizzBuzz {
+
+    // This program prints the first 100 outputs for the basic coding problem "FizzBuzz"
+    // if multiple of 3 & 7, print "FizzBuzz"
+    // else if multiple of 3, print "Fizz"
+    // else if multiple of 7, print "Buzz"
+    // else print number
+
+
+}
