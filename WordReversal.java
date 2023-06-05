bad1ba0a92011d3138e8740f2750757073a8c57d
HaydenChan8619 <haydenphchan@gmail.com>
2023-06-05 12:18:32 -0700

    This is the first commit with the comments written.

---
+++ WordReversal.java
@@ -0,0 +1,5 @@
+public class WordReversal {
+
+    // The following code will flip the order of the words in an inputted sentence (based on spaces)
+    // Example: Input "Dog is good" ; Output "good is Dog"
+}
